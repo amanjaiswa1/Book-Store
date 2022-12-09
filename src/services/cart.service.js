ed83da04a8cbb1b6859127405c22da47760a6fe6
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-12-09 19:48:49 +0530

    [Aman] Modified: Book Schema & Adding Book Details To Cart

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -4,6 +4,14 @@ import Book from '../models/book.model';
 //create a new user registration
 export const addBookToCart = async (body) => {
     const findBook = await Book.findOne({ _id: body._id });
+    let updateBookDetails = {
+        productID: findBook._id,
+        description: findBook.description,
+        bookName: findBook.bookName,
+        bookImage: findBook.bookImage,
+        author: findBook.author,
+        price: findBook.price
+    };
     if (findBook != null) {
         const findCart = await Cart.findOne({ userID: body.userID });
         if (findCart != null) {
@@ -11,26 +19,15 @@ export const addBookToCart = async (body) => {
                 {
                     _id: findCart._id
                 },
-                {
-                    $push: { books: { productID: findBook._id } }
-                },
+                { $push: { books: [updateBookDetails] } },
                 {
                     new: true
-                });
+                }
+            );
             return addBookInCart;
         } else {
-            const createNewCart = await Cart.create({ userID: body.userID });
-            const addBookInCart = await Cart.findOneAndUpdate(
-                {
-                    userID: body.userID
-                },
-                {
-                    $push: { books: { productID: findBook._id } }
-                },
-                {
-                    new: true
-                });
-            return addBookInCart;
+            const createNewCart = await Cart.create({ userID: body.userID, books: [updateBookDetails], cartTotal: findBook.price });
+            return createNewCart;
         }
     } else {
         throw new Error("Book not found!!!")
