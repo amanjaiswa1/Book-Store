31e59cdb79af880d883c2ccff62ce9627ca0a380
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-12-11 22:15:30 +0530

    [Aman] Added: Remove Book From Cart

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -7,4 +7,7 @@ const router = express.Router();
 //route to add book to cart
 router.post('', userCartAuth, cartController.addBookToCart);
 
+//route to remove book from cart
+router.post('/removebook', userCartAuth, cartController.removeBookFromCart);
+
 export default router;
--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -57,3 +57,44 @@ export const addBookToCart = async (body) => {
         throw new Error("Book not found!!!")
     }
 };
+
+//create a new user registration
+export const removeBookFromCart = async (body) => {
+    const findBook = await Book.findOne({ _id: body._id });
+    let bookMatchFound = false;
+    let calculatePrice = 0, removedBookPrice = 0;
+    if (findBook != null) {
+        const findCart = await Cart.findOne({ userID: body.userID });
+        if (findCart != null) {
+            findCart.books.forEach(object => {
+                if (object.productID == body._id) {
+                    removedBookPrice = object.price * object.quantity;
+                    calculatePrice = findCart.cartTotal - removedBookPrice;
+                    findCart.books.splice(findCart.books.indexOf(object), 1);
+                    bookMatchFound = true;
+                }
+            });
+            if (bookMatchFound == true) {
+                const removeBookFromCart = await Cart.findOneAndUpdate(
+                    {
+                        _id: findCart._id
+                    },
+                    { books: findCart.books, cartTotal: calculatePrice },
+                    {
+                        new: true
+                    }
+                );
+                return removeBookFromCart;
+            }
+            else {
+                throw new Error("Book does not exist in cart")
+            }
+        }
+        else {
+            throw new Error("Cart does not exist!!!")
+        }
+    }
+    else {
+        throw new Error("Book not found!!!")
+    }
+};
