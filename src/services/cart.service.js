5e52312c4f7d10674079fd0e49c0afd54a7ed724
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-12-11 13:20:25 +0530

    [Aman] Modified: To Add More Than One Book Quantity And Cart Total Price

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -4,6 +4,8 @@ import Book from '../models/book.model';
 //create a new user registration
 export const addBookToCart = async (body) => {
     const findBook = await Book.findOne({ _id: body._id });
+    let bookMatchFound = false;
+    let calculatePrice = 0;
     let updateBookDetails = {
         productID: findBook._id,
         description: findBook.description,
@@ -15,16 +17,38 @@ export const addBookToCart = async (body) => {
     if (findBook != null) {
         const findCart = await Cart.findOne({ userID: body.userID });
         if (findCart != null) {
-            const addBookInCart = await Cart.findOneAndUpdate(
-                {
-                    _id: findCart._id
-                },
-                { $push: { books: [updateBookDetails] } },
-                {
-                    new: true
+            findCart.books.forEach(object => {
+                if (object.productID == body._id) {
+                    object.quantity += 1;
+                    calculatePrice = object.price + findCart.cartTotal;
+                    bookMatchFound = true;
                 }
-            );
-            return addBookInCart;
+            });
+            if (bookMatchFound == true) {
+                const addBookInCart = await Cart.findOneAndUpdate(
+                    {
+                        _id: findCart._id
+                    },
+                    { books: findCart.books, cartTotal: calculatePrice },
+                    {
+                        new: true
+                    }
+                );
+                return addBookInCart;
+            }
+            else {
+                findCart.cartTotal += updateBookDetails.price;
+                const addBookInCart = await Cart.findOneAndUpdate(
+                    {
+                        _id: findCart._id
+                    },
+                    { $push: { books: [updateBookDetails] }, cartTotal: findCart.cartTotal },
+                    {
+                        new: true
+                    }
+                );
+                return addBookInCart;
+            }
         } else {
             const createNewCart = await Cart.create({ userID: body.userID, books: [updateBookDetails], cartTotal: findBook.price });
             return createNewCart;
